c68fccc2ef2a1b40c414c25782f0cb648463321e
DrMaul <agusfmartinez99@gmail.com>
2024-04-09 15:18:33 -0300

    clase 9

--- clase 9/02-Multer/src/app.js
+++ clase 9/02-Multer/src/app.js
@@ -1,60 +1,18 @@
-import express from "express"
-import {router as usuariosRouter} from './routes/usuariosRouter.js'
-import {middleware01, middleware02, middleware03} from './middlewares/generales.js'
-import {auth} from './middlewares/auth.js' 
-import { errorHandler } from "./middlewares/errorHandler.js"
+import express from 'express';
+import { router as heroesRouter } from './routes/heroesRouter.js';
+const PORT=3000;
 
-const PORT = 3000
-const app = express()
+const app=express();
 
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
-app.use("/api/usuarios", usuariosRouter)
+app.use(express.static("./src/public"))
 
-app.use(
-    middleware01, 
-    middleware02,
-    (req, res, next)=> {
-        console.log('middleware a nivel app "online"')
-        next()
-    })
-  
-
-app.get("/", middleware03,(req, res)=> {
-
-    res.setHeader('Content-Type', 'text/plain')
-    res.status(200).send('OK')
-})
-
-app.get("/datos", middleware03,(req, res)=> {
-
-    res.setHeader('Content-type', 'application/json')
-    return res.status(200).json({
-        payload:"Datos",
-        nombre: req.query.nombre,
-        codigo: req.codigo
-    })
-})
-
-app.get("/datos2", middleware03,auth,(req, res)=> {
-
-    res.setHeader('Content-type', 'application/json')
-    return res.status(200).json({
-        payload:"Datos2",
-        nombre: req.query.nombre,
-        codigo: req.codigo
-    })
-})
-
-app.get("/datos3", middleware03,(req, res)=> {
-
-    console.log(lalala)
-    res.setHeader('Content-Type', 'text/plain')
-    res.status(200).send('OK')
-})
-
-app.use(errorHandler)
+app.use('/api/heroes', heroesRouter)
 
 
-app.listen(PORT, ()=>console.log(`Servidor online en puerto ${PORT}`))
+
+const server=app.listen(PORT,()=>{
+    console.log(`Server escuchando en puerto ${PORT}`);
+});
--- clase 9/02-Multer/src/utils.js
+++ clase 9/02-Multer/src/utils.js
@@ -1,15 +1,27 @@
-import {multer} from 'multer'
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import multer from 'multer'
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export default __dirname;
 
 const storage = multer.diskStorage({
-    destination: function (req,file,cb){
+    destination: function (req, file, cb) {
         cb(null, './src/uploads')
     },
-    filename: function (req, file, cb){
-       // const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-        //cb(null, file.fieldname + '-' + uniqueSuffix)
+    filename: function (req, file, cb) {
+        // const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+        // cb(null, file.fieldname + '-' + uniqueSuffix)
+        let tipo = file.mimetype.split("/")[0]
+        if(tipo !=="image"){
+            return cb(new Error("Solo se admiten imagenes"))
+        }
+
 
         cb(null, Date.now() + "-" + file.originalname)
     }
 })
 
-export const upload = multer({storage:storage})
+export const upload = multer({ storage: storage })
