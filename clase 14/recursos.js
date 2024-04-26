275b2bc730db28ce991d22717a5671887a650cc5
DrMaul <agusfmartinez99@gmail.com>
2024-04-26 17:45:34 -0300

    clase 14

---
+++ clase 14/recursos.js
@@ -0,0 +1,8 @@
+
+
+//Datos Mongo Atlas
+//user: agusfmartinez
+//passwrod: CoderCoder
+
+//
+// mongodb+srv://agusfmartinez:CoderCoder@cluster0.zvgrerx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
