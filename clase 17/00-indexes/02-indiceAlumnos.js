5535985c3d30776c728cfad2d9ac766dd6ba9506
DrMaul <agusfmartinez99@gmail.com>
2024-05-15 02:13:01 -0300

    clase 17

--- clase 17/00-indexes/02-indiceAlumnos.js
+++ clase 17/00-indexes/02-indiceAlumnos.js
@@ -15,7 +15,8 @@ let alumnoModelo=mongoose.model('alumnos', alumnoEsquema)
 
 alumnoEsquema.index({nombre:-1})
 alumnoEsquema.index({nombre:-1, apellido: 1})
-
+alumnoEsquema.index({email:-1})
+alumnoEsquema.index({email:"text"})
 
 const entorno = async () => {
 
@@ -23,9 +24,14 @@ const entorno = async () => {
         await mongoose.connect(mongourl)
         console.log("Conexión a la DB establecida")
 
-        let resultado = await alumnoModelo.find({nombre: "Martina", apellido: "Souto"}).explain("executionStats")
-        console.log(JSON.stringify(resultado.executionStats,null,5))
+        /* let resultado = await alumnoModelo.find({nombre: "Martina", apellido: "Souto"}).explain("executionStats")
+        console.log(JSON.stringify(resultado.executionStats,null,5)) */
 
+        /* let resultado = await alumnoModelo.find({email:/hotmail/}).explain("executionStats")
+        console.log(JSON.stringify(resultado.executionStats,null,5)) */
+
+        let resultado = await alumnoModelo.find({$text:{$search:/hotmail/}}).explain("executionStats")
+        console.log(JSON.stringify(resultado.executionStats,null,5))
 
         process.exit()
     } catch (error) {
