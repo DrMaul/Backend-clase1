0d03e1aa272d0aafdc9a933d8d9e416184863c2e
DrMaul <agusfmartinez99@gmail.com>
2024-06-20 17:05:44 -0300

    after 9

---
+++ after 9/src/controllers/negociosController.js
@@ -0,0 +1,47 @@
+import { NegociosDAO } from "../dao/NegociosDAO.js";
+
+
+const negociosService = new NegociosDAO()
+
+export const getNegocios = async (req,res)=> {
+
+    // let negocios = "todos los negocios"
+    let negocios = await negociosService.get()
+
+    res.setHeader('Content-Type','application/json');
+    return res.status(200).json({negocios});
+}
+
+export const createNegocio = async (req,res)=> {
+    let {nombre, productos} = req.body
+
+    if(!nombre || !productos || !Array.isArray(productos)){
+        res.setHeader('Content-Type','application/json');
+        return res.status(400).json({error:`Complete nombre / email`})
+    }
+
+    //validar existencia de negocio repetido
+    let existe = await negociosService.getOneBy({nombre})
+    if(existe){
+        res.setHeader('Content-Type','application/json');
+        return res.status(400).json({error:`El negocio ${nombre} ya existe en la BBDD`})
+    }
+    
+    // otras validaciones
+    try {
+        // let nuevoNegocio = `nuevo negocio ${nombre}`
+        let nuevoNegocio = await negociosService.create({nombre, productos})
+        res.setHeader('Content-Type','application/json');
+        return res.status(201).json({nuevoNegocio});
+        
+    } catch (error) {
+        res.setHeader('Content-Type','application/json');
+        return res.status(500).json(
+            {
+                error:`Error inesperado en el servidor - Intente más tarde, o contacte a su administrador`,
+                detalle:`${error.message}`
+            }
+        )
+        
+    }
+}
